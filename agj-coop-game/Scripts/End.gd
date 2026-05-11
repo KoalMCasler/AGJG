68b4f1eaddece077dcfe27c0d63e3f880ee01f78
Koal M. Casler <Theeoblivion@gmail.com>
2026-05-10 23:34:32 -0300

    Beat counter and tile map

---
+++ agj-coop-game/Scripts/End.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+
+
+func _on_PlayAgain_pressed():
+	if get_tree().change_scene_to_file("res://Scenes/Game.tscn") != OK:
+			print ("Error changing scene to Game")
+
+
+func _on_BackToMenu_pressed():
+	if get_tree().change_scene_to_file("res://Scenes/Menu.tscn") != OK:
+			print ("Error changing scene to Menu")
